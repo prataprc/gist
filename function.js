2992d5b85eb8c2ebcef8a6fbb4d7ce577240569b
Pratap Chakravarthy <prataprc@gmail.com>
2013-06-23 19:44:53 +0530

    More gist.

--- function.js
+++ function.js
@@ -1,6 +1,13 @@
 foo();              // Works because foo was created before this code runs
 function foo() {}
 
+//----------------
 foo;    // 'undefined'
 foo();  // this raises a TypeError
 var foo = function() {};
+
+//-----------------
+var foo = function bar() {
+    bar();  // Works
+}
+bar();      // ReferenceError
