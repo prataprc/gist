69a2c1ce0811cd56cba5bf346ee21376756f2700
Pratap Chakravarthy <prataprc@gmail.com>
2013-06-24 08:08:25 +0530

    More gist.

--- eval.js
+++ eval.js
@@ -1,5 +1,12 @@
 var a = 10;
 
+// expressions can be evaluated, returns the value.
+eval('10*20')       // => 200
+// assignments can be evaluated, return assigned value.
+eval('x=100')       // => 100
+// even statements can be evaluated
+eval('if(true) { 100 } else {200}') // => 100
+
 function bar(s) {
     var a = 100;
     eval(s);
