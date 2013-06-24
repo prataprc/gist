821e348a697859faf001f5fa6bfaca666d177531
Pratap Chakravarthy <prataprc@gmail.com>
2013-06-24 08:08:25 +0530

    More gist.

--- ops_assign.js
+++ ops_assign.js
@@ -9,6 +9,8 @@ i /= 3;     // 2
 i %= 2;     // 0
 
 // assignments create side effects and also treated as expression,
+(x=10) * 200    // => 2000
+x+20            // => 30
 function bar() { return [ a=10, b="hello" ]; }
 [a, b, bar()]   // Error
 [bar(), a, b]   // => [[10, "hello"], 10, "hello"]
