faf935e55d3de0fec579c8c3c1075f96f3c59b37
prataprc <prataprc@gmail.com>
2014-05-15 12:20:08 +0530

    Few more examples.

--- gctest.go
+++ gctest.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"time"
+    "flag"
+    "fmt"
+    "time"
 )
 
 var options struct {
-	n       int // number of parallel go-routines
-	maxsize int // Maximum block size to allocate
-	loop    int // number allocations
+    n       int // number of parallel go-routines
+    maxsize int // Maximum block size to allocate
+    loop    int // number allocations
 }
 
 func main() {
-	flag.IntVar(&options.n, "n", 1, "Number of parallel routines")
-	flag.IntVar(&options.maxsize, "maxsize", 1000, "maximum size to allocate")
-	flag.IntVar(&options.loop, "loop", 1000000, "number of allocations")
-	flag.Parse()
-	ch := make(chan bool)
-	count := 200000 * 1000 / options.maxsize
-	fmt.Println(count)
-	for i := 0; i < options.n; i++ {
-		go alloc(options.loop, int64(count), int64(options.maxsize), ch)
-	}
-	for i := 0; i < options.n; i++ {
-		<-ch
-	}
+    flag.IntVar(&options.n, "n", 1, "Number of parallel routines")
+    flag.IntVar(&options.maxsize, "maxsize", 1000, "maximum size to allocate")
+    flag.IntVar(&options.loop, "loop", 1000000, "number of allocations")
+    flag.Parse()
+    ch := make(chan bool)
+    count := 200000 * 1000 / options.maxsize
+    fmt.Println(count)
+    for i := 0; i < options.n; i++ {
+        go alloc(options.loop, int64(count), int64(options.maxsize), ch)
+    }
+    for i := 0; i < options.n; i++ {
+        <-ch
+    }
 }
 
 func alloc(loopcount int, count, maxsize int64, ch chan bool) [][]byte {
-	var sl [][]byte
-	for i := int64(0); i < count; i++ {
-		sl = append(sl, make([]byte, i%maxsize))
-	}
-	for i := 0; i < loopcount; i++ {
-		sl = append(sl, make([]byte, time.Now().UnixNano()%maxsize))
-		sl = sl[1:]
-	}
-	ch <- true
-	return sl
+    var sl [][]byte
+    for i := int64(0); i < count; i++ {
+        sl = append(sl, make([]byte, i%maxsize))
+    }
+    for i := 0; i < loopcount; i++ {
+        sl = append(sl, make([]byte, time.Now().UnixNano()%maxsize))
+        sl = sl[1:]
+    }
+    ch <- true
+    return sl
 }
