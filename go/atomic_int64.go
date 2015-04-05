7b8eadf366420aa6af7a5766e1b1226f3f2ee192
prataprc <prataprc@gmail.com>
2015-04-05 20:37:25 +0530

    golang snippet to demonstrate unaligned atomic access.
    tested on a 32 bit machine.

--- go/atomic_int64.go
+++ go/atomic_int64.go
@@ -4,16 +4,20 @@ import "fmt"
 import "sync/atomic"
 
 type X struct {
-    a  chan bool
-    b  chan bool
-    c  *int64
-    d  string
-    e uint64
+	z uint64
+	a chan bool
+	b chan bool
+	c *int64
+	d string
+	e uint64
 }
 
 func main() {
-    s := X{}
-    fmt.Println(s.e)
-    atomic.AddUint64(&s.e, 1)
-    fmt.Println(s.e)
+	s := X{}
+	fmt.Println(s.z)
+	atomic.AddUint64(&s.z, 1)
+	fmt.Println(s.z)
+	fmt.Println(s.e)
+	atomic.AddUint64(&s.e, 1)
+	fmt.Println(s.e)
 }
