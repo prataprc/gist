63db12d8b0a359eb333930a13ca70fcef9addcd7
prataprc <prataprc@gmail.com>
2015-04-07 13:43:43 +0000

    go: argument alignment in function call

--- go/atomic_align.go
+++ go/atomic_align.go
@@ -1,6 +1,7 @@
 package main
 
 import "sync/atomic"
+import "fmt"
 
 type X struct {
 	a       uint64 // will be 64-bit aligned, no panic
@@ -19,22 +20,25 @@ type X struct {
 	}
 }
 
-func alignedCall(m map[string]uint64, i int, counters [257]uint64) uint64 {
-	v := atomic.LoadUint64(&counters[i]) // Will counters be 64-bit aligned
+// arguments are always 64-bit aligned in stack.
+func alignedCall(b bool, i uint64) uint64 {
+	v := atomic.LoadUint64(&i)
+	fmt.Printf("alignedCall(%p, %p)\n", &b, &i)
 	return v
 }
 
 func main() {
 	s := X{}
 
-	atomic.AddUint64(&s.a, 1)   // no panic
+	atomic.AddUint64(&s.a, 1) // no panic
 	//atomic.AddUint64(&s.c, 1) // panic on 32-bit machine
-	s.e = new(uint64)           // on 32-bit mach, will it be 64-bit aligned ??
-	atomic.AddUint64(s.e, 1)    // no panic, if 64-bit aligned
+	s.e = new(uint64)        // on 32-bit mach, will it be 64-bit aligned ??
+	atomic.AddUint64(s.e, 1) // no panic, if 64-bit aligned
 	// atomic.AddUint64(&s.unalign.j, 1) // panic on 32-bit machine
 	atomic.AddUint64(&s.align.m, 1) // no panic
 	//atomic.AddUint64(&s.align.o, 1) // panic on 32-bit machine
-	m := map[string]uint64{"a": 10}
-	counters := [257]uint64{}
-	alignedCall(m, 10, counters)
+	y := uint64(10)
+	x := alignedCall(true, y)
+	fmt.Printf("%p: %v\n", &y, y)
+	fmt.Printf("%p: %v\n", &x, x)
 }
