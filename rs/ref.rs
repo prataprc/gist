829595da6fbd2aca794a71c13979763d6aa02e3c
prataprc <prataprc@gmail.com>
2015-06-04 17:39:56 +0530

    `ref` in rs.

--- rs/ref.rs
+++ rs/ref.rs
@@ -5,4 +5,12 @@ fn main() {
     println!("reference {:p}", stdin);
     println!("pointer to reference {:p}", &stdin);
     println!("boxed heap at {:p}", Box::new(10));
+
+    refs();
+}
+
+fn refs() {
+    let x = &10;
+    let ref y = x;
+    println!("x={:p} &x={:p}, y={:p}, &y={:p}", x, &x, y, &y);
 }
