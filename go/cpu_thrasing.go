325e78a8ed5cbfb31d9e0adbb1eda0ce8084abfa
prataprc <prataprc@gmail.com>
2015-09-18 13:34:00 +0530

    golang: cpu thrashing.

--- go/cpu_thrasing.go
+++ go/cpu_thrasing.go
@@ -1,6 +1,20 @@
 package main
 
+import "time"
+import "runtime"
+import "fmt"
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Println("cpus: ", runtime.NumCPU())
+	n := 10
+	for i := 0; i < n; i++ {
+		go percore()
+	}
+	time.Sleep(1 * time.Hour)
+}
+
+func percore() {
 	i := 0
 	for {
 		i++
