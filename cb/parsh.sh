5e8a83d2c1de276c226d99e210049c3c276f5325
prataprc <prataprc@gmail.com>
2016-02-28 11:10:17 +0530

    cb: script for parallel command execution.

---
+++ cb/parsh.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#if [ "$1" = "" ]; then
+#  echo 'Usage: cluster_ssh "ip1 ip2 ip3..." "command"'
+#  exit 0
+#fi
+#
+#for i in $1; do
+#  echo "Running on $i: $2"
+#  #sshpass -p couchbase ssh -q -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" root@$i "$2"
+#  ssh -q -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" prataprc@$i "$2"
+#done
+
+if [ "$1" = "" ]; then
+  echo 'Usage: cluster_ssh "ip1 ip2 ip3..." "command"'
+  exit 0
+fi
+
+for i in $1; do
+  echo "$i: Running $2"
+  #(sshpass -p couchbase ssh -q -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" root@$i "$2" 2>&1 | sed "s/^/$i: /") &
+  (ssh -q -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" couchbase@$i "$2" 2>&1 | sed "s/^/$i: /") &
+done
+
+wait
