1bd19162fbe0eb483e86c9de910c2f3a378e394c
prataprc <prataprc@gmail.com>
2017-08-24 16:48:30 +0530

    couchbase scripts.

---
+++ cb/cbconnect.sh
@@ -0,0 +1,1 @@
+./cluster_connect -n2 -s 2048 -I 2048 -T n0:kv+n1ql,n1:index
---
+++ cb/cbrun.sh
@@ -0,0 +1,8 @@
+pkill indexer
+pkill projector
+pkill gometa
+pkill beam.smp
+pkill epmd
+
+make dataclean
+~/0cb/ns_server/cluster_run --nodes=2
