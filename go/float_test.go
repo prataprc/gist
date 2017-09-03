ff709197cb51db5be838a97632fa158249ad1a12
prataprc <prataprc@gmail.com>
2017-09-03 18:27:42 +0530

    go: Benchmark f32 and f64.

--- go/float_test.go
+++ go/float_test.go
@@ -3,6 +3,20 @@ package mem
 import "testing"
 import "strconv"
 
+func BenchmarkSinglePrecision(b *testing.B) {
+	acc := float32(0)
+	for i := 0; i < b.N; i++ {
+		acc += float32(2.2)
+	}
+}
+
+func BenchmarkDoublePrecision(b *testing.B) {
+	acc := float64(0)
+	for i := 0; i < b.N; i++ {
+		acc += float64(2.2222222222)
+	}
+}
+
 func BenchmarkParseFloat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		strconv.ParseFloat("10.312341243532534", 64)
