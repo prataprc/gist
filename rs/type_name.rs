85cca961b5c404275aaa713b831e81a316659152
prataprc <prataprc@gmail.com>
2018-11-29 17:35:58 +0530

    rs: type name.

--- rs/type_name.rs
+++ rs/type_name.rs
@@ -8,4 +8,8 @@ type MyType<T> = Other<T>;
 fn main() {
     let x: MyType<u32> = Other(19, 29, 20);
     println!("my type {:?}", x);
+
+    let x = b"hello world";
+    let y: &str = x;
+    println!("my type {:?}", y);
 }
