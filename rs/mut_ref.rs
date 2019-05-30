23dbc234a8a829e4432bc7cadff40976ac697f9e
prataprc <prataprc@gmail.com>
2019-05-30 10:39:25 +0530

    rs: examples.

--- rs/mut_ref.rs
+++ rs/mut_ref.rs
@@ -1,13 +1,36 @@
-struct Ty { x: i32, list: Vec<Ty> }
+struct Ty {
+    x: i32,
+    list: Vec<Ty>,
+}
+
+struct Tx<'a> {
+    field: &'a mut Ty,
+}
 
 fn main() {
-    let mut a = Ty{x: 10, list: vec![Ty{x: 20, list:vec![]}, Ty{x: 20, list:vec![]}]};
-    println!("{:p}", get(&mut a, 1))
+    let mut a = Ty {
+        x: 10,
+        list: vec![
+            Ty {
+                x: 20,
+                list: vec![],
+            },
+            Ty {
+                x: 20,
+                list: vec![],
+            },
+        ],
+    };
+    println!("{:p}", get(&mut a, 1));
+
+    let b = Tx { field: &mut a };
+    b.field.x = 10;
+    println!("{}", b.field.x);
 }
 
 fn get(mut ptr: &mut Ty, idx: usize) -> &mut Ty {
     for i in 0..idx {
         ptr = &mut ptr.list[i]
     }
-    return ptr
+    return ptr;
 }
---
+++ rs/orphan_param.rs
@@ -0,0 +1,22 @@
+//Type annotation for associated constants.
+//
+//I have a situation where I need to club related constants with a type
+//and the type is parametrised over `V`. Now when I use the constant
+//outside the type's impl, like the example given below:
+//
+//```
+struct Value<V> {
+    value: V,
+}
+
+impl<V> Value<V> {
+    const VALUE_FLAG: u64 = 0x1000000000000000;
+}
+
+fn main() {
+    let x = 0x20 | Value::VALUE_FLAG;
+    println!("{}", x);
+}
+//```
+//
+//I get following error:
