0c8001cd4ebfdfab9c4a68a5d7dcaa5aa2e76867
prataprc <prataprc@gmail.com>
2020-05-22 19:17:32 +0530

    rs: associated type for trait.

--- rs/associated.rs
+++ rs/associated.rs
@@ -18,8 +18,27 @@ impl<'a> MyAdd<&'a f64> for X {
     fn add(&self, rhs: &f64) -> Self::Sum { self.a + (*rhs as i32) }
 }
 
+trait Window {
+    type Msg;
+
+    fn notify(&self, Self::Msg) -> Result<(), String> {
+        Ok(())
+    }
+}
+
+impl Window for X {
+    type Msg = u64;
+
+    fn notify(&self, msg: u64) -> Result<(), String> {
+        Ok(())
+    }
+}
+
 fn main() {
     let x = X{a: 10};
     println!("{:?}", x);
     x.add((&20).clone());
+
+    let y: Box<dyn Window<Msg=u64>> = Box::new(X{ a: 10 });
+    y.notify(20);
 }
